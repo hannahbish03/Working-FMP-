998993741b335af317273b5d3236878a171803d4
hannahbish03 <71505517+hannahbish03@users.noreply.github.com>
2022-04-22 12:00:23 +0100

    UpandDown script

---
+++ FMP 18th March 22/Assets/Scripts/UpandDown2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpandDown2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+    
+        //adjust this to change speed
+        float speed = 2f;
+        //adjust this to change how high it goes
+        float height = 1f;
+
+        void Update()
+        {
+            //get the objects current position and put it in a variable so we can access it later with less code
+            Vector3 pos = transform.position;
+            //calculate what the new Y position will be
+            float newY = Mathf.Sin(Time.time * speed);
+            //set the object's Y to the new calculated Y
+            transform.position = new Vector3(pos.x, newY, pos.z);
+        }
+    
+}
--- FMP 18th March 22/Assets/Scripts/upAndDown.cs
+++ FMP 18th March 22/Assets/Scripts/upAndDown.cs
@@ -4,33 +4,64 @@ using UnityEngine;
 
 public class upAndDown : MonoBehaviour
 {
-    public GameObject[] waypoints;
-    int current = 0;
-    float rotSpeed;
-    public float speed;
-    float WPradius = 1;
-   
-    
-    // Start is called before the first frame update
-  
+    //    public GameObject[] waypoints;
+    //    int current = 0;
+    //    float rotSpeed;
+    //    public float speed;
+    //    float WPradius = 1;
 
-    // Update is called once per frame
+
+    //    // Start is called before the first frame update
+
+
+    //    // Update is called once per frame
+    //    void Update()
+    //    {
+    //        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius) {
+    //            current++;
+
+
+    //            current = Random.Range(0, waypoints.Length);
+    //            if (!(current >= waypoints.Length)){
+
+    //                current = 0;
+
+    //            }
+
+    //            transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    //        }
+
+
+    //    }
+
+
+
+    //adjust this to change speed
+    float speed = 5f;
+    //adjust this to change how high it goes
+    float height = 0.5f;
+
     void Update()
     {
-        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius) {
-            current++;
-
-
-            current = Random.Range(0, waypoints.Length);
-            if (!(current >= waypoints.Length)){
-
-                current = 0;
-            
-            }
-
-            transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
-        }
-    
-    
+        //get the objects current position and put it in a variable so we can access it later with less code
+        Vector3 pos = transform.position;
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed);
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(pos.x, newY, pos.z);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
