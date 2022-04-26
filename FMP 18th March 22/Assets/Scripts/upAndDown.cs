efdfd12c5de843b7e2e2c4ef318229264bd87706
hannahbish03 <71505517+hannahbish03@users.noreply.github.com>
2022-04-26 11:21:28 +0100

    fixing up and down script

--- FMP 18th March 22/Assets/Scripts/upAndDown.cs
+++ FMP 18th March 22/Assets/Scripts/upAndDown.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class upAndDown : MonoBehaviour
 {
+    //adjust this to change speed
+    public float speed = 5f;
+    //adjust this to change how high it goes
+    public float height = 0.5f;
+
     //    public GameObject[] waypoints;
     //    int current = 0;
     //    float rotSpeed;
@@ -12,6 +17,13 @@ public class upAndDown : MonoBehaviour
 
 
     //    // Start is called before the first frame update
+
+    private float startY;
+
+    private void Awake()
+    {
+        startY = transform.position.y;
+    }
 
 
     //    // Update is called once per frame
@@ -34,19 +46,12 @@ public class upAndDown : MonoBehaviour
 
     //    }
 
-
-
-    //adjust this to change speed
-    float speed = 5f;
-    //adjust this to change how high it goes
-    float height = 0.5f;
-
     void Update()
     {
         //get the objects current position and put it in a variable so we can access it later with less code
         Vector3 pos = transform.position;
         //calculate what the new Y position will be
-        float newY = Mathf.Sin(Time.time * speed);
+        float newY = (Mathf.Sin(Time.time * speed) * height) + startY;
         //set the object's Y to the new calculated Y
         transform.position = new Vector3(pos.x, newY, pos.z);
     }
