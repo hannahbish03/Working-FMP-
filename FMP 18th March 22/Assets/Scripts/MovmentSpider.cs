7abca18a15fa6eb54170c305d61e9e15f405fe29
hannahbish03 <71505517+hannahbish03@users.noreply.github.com>
2022-04-28 10:41:12 +0100

    NavMesh

--- FMP 18th March 22/Assets/Scripts/MovmentSpider.cs
+++ FMP 18th March 22/Assets/Scripts/MovmentSpider.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MovmentSpider : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    GameManager gameManager;
+    GameManager gameManager;    
+    public Transform goal;
 
     void Start()
     {
         GameObject gameController = GameObject.FindGameObjectWithTag("spider_pref");
         gameManager = gameController.GetComponent<GameManager>();
+
+        //NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        //agent.destination = goal.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
-        transform.Translate(gameManager.moveVector * gameManager.moveSpeed * Time.deltaTime);
+        //transform.Translate(gameManager.moveVector * gameManager.moveSpeed * Time.deltaTime);
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
     }
+
+
+
 }
+
